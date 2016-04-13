01aa199b3e6093e9654ef9d97ab1b3054b86262e
Dave Whipp <david.whipp@helsinki.fi>
2016-04-13 13:51:59 +0300

    Make nicer plot axis ranges

--- make_subplot.py
+++ make_subplot.py
@@ -20,6 +20,7 @@ plt.subplot(2, 2, 1)
 plt.plot(x, y1, 'ro-')
 plt.xlabel('angle [rad.]')
 plt.ylabel('sin(x)')
+plt.axis([0.0, 2 * np.pi, -1.1, 1.1])
 plt.title('Example of four subplots')
 
 # Create subplot 2
@@ -27,18 +28,21 @@ plt.subplot(2, 2, 2)
 plt.plot(x, y2, 'ko-')
 plt.xlabel('angle [rad.]')
 plt.ylabel('cos(x)')
+plt.axis([0.0, 2 * np.pi, -1.1, 1.1])
 
 # Create subplot 3
 plt.subplot(2, 2, 3)
 plt.plot(x, y3, 'bo-')
 plt.xlabel('angle [rad.]')
 plt.ylabel('sin(x^2)')
+plt.axis([0.0, 2 * np.pi, -1.1, 1.1])
 
 # Create subplot 4
 plt.subplot(2, 2, 4)
 plt.plot(x, y4, 'go-')
 plt.xlabel('angle [rad.]')
 plt.ylabel('cos(x^2)')
+plt.axis([0.0, 2 * np.pi, -1.1, 1.1])
 
 # Display plot
 plt.show()
