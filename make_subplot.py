42271af9aa49fcd43e0f1fc75ee86d52fbc4cac2
Dave Whipp <david.whipp@helsinki.fi>
2016-04-13 13:46:49 +0300

    Fixed plots to have 4 subplots

--- make_subplot.py
+++ make_subplot.py
@@ -16,26 +16,26 @@ y3 = np.cos(x**2)
 y4 = np.cos(x**2)
 
 # Create subplot 1
-plt.subplot(2, 1, 1)
+plt.subplot(2, 2, 1)
 plt.plot(x, y1, 'ro-')
 plt.xlabel('angle [rad.]')
 plt.ylabel('sin(x)')
 plt.title('Example of two subplots')
 
 # Create subplot 2
-plt.subplot(2, 1, 2)
+plt.subplot(2, 2, 2)
 plt.plot(x, y2, 'ko-')
 plt.xlabel('angle [rad.]')
 plt.ylabel('cos(x)')
 
 # Create subplot 3
-plt.subplot(2, 1, 2)
+plt.subplot(2, 2, 3)
 plt.plot(x, y3, 'ko-')
 plt.xlabel('angle [rad.]')
 plt.ylabel('sin(x^2)')
 
 # Create subplot 4
-plt.subplot(2, 1, 2)
+plt.subplot(2, 2, 4)
 plt.plot(x, y4, 'ko-')
 plt.xlabel('angle [rad.]')
 plt.ylabel('cos(x^2)')
